cc33e8f6cb359135b96e4ca818a2594560e8ddb1
gglyptodon <janina.mass@gmail.com>
2024-06-15 19:44:18 -0400

    stage2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -10,4 +9,20 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    let known_commands: Vec<&str> = Vec::new();
+
+    let _command = match check_exists(&input, &known_commands) {
+        Ok(cmd) => cmd,
+        Err(e) => {
+            eprintln!("{}", e);
+            ""
+        }
+    };
+}
+fn check_exists<'a>(user_input: &'a str, known_commands: &Vec<&str>) -> Result<&'a str, String> {
+    if !known_commands.contains(&user_input.trim_end()) {
+        return Err(format!("{}: command not found", &user_input.trim_end()));
+    }
+    Ok(user_input)
 }
