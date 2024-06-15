e5914d4a02b34cf15f3cb2cfa042858b16c0d36b
gglyptodon <janina.mass@gmail.com>
2024-06-15 19:53:15 -0400

    stage3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 use std::io::{self, Write};
 
 fn main() {
+    let known_commands: Vec<&str> = Vec::new();
+
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -10,15 +12,13 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    let known_commands: Vec<&str> = Vec::new();
-
-    let _command = match check_exists(&input, &known_commands) {
-        Ok(cmd) => cmd,
-        Err(e) => {
-            eprintln!("{}", e);
-            ""
-        }
-    };
+    while let Err(e) = check_exists(&input, &known_commands) {
+        eprintln!("{}", e);
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        input = String::new();
+        stdin.read_line(&mut input).unwrap();
+    }
 }
 fn check_exists<'a>(user_input: &'a str, known_commands: &Vec<&str>) -> Result<&'a str, String> {
     if !known_commands.contains(&user_input.trim_end()) {
