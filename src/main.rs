067efa7ce8f7ca1b7a04b7769981f67a6de4388a
gglyptodon <janina.mass@gmail.com>
2024-06-15 20:51:58 -0400

    stage5

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::{self, Write};
 fn main() {
     let mut known_commands: Vec<&str> = Vec::new();
     known_commands.push("exit");
+    known_commands.push("echo");
 
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -21,15 +22,18 @@ fn main() {
         input = String::new();
         stdin.read_line(&mut input).unwrap();
     }
-    if let Ok(_cmd) = check_exists(&input, &known_commands) {
-        //println!("{}", cmd.join(" "));
+    if let Ok(cmd) = check_exists(&input, &known_commands) {
+        match cmd.first().unwrap(){
+            &"echo" => println!("{}", cmd.join(" ")),
+            _ => {}
+        }
     }
 }
 fn check_exists<'a>(
     user_input: &'a str,
     known_commands: &Vec<&str>,
 ) -> Result<Vec<&'a str>, String> {
-    let mut input = user_input.trim().split_whitespace().collect::<VecDeque<&str>>();
+    let mut input = user_input.split_whitespace().collect::<VecDeque<&str>>();
     let cmd = input.pop_front().unwrap();
     let mut params = Vec::from(input.clone());
     if !known_commands.contains(&cmd) {
