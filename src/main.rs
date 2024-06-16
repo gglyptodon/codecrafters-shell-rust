73a73c35ff2cddf2e5600c4aeb81fbdd5c620eb7
gglyptodon <janina.mass@gmail.com>
2024-06-15 20:42:57 -0400

    stage4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::collections::VecDeque;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    let known_commands: Vec<&str> = Vec::new();
+    let mut known_commands: Vec<&str> = Vec::new();
+    known_commands.push("exit");
 
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -19,10 +21,22 @@ fn main() {
         input = String::new();
         stdin.read_line(&mut input).unwrap();
     }
-}
-fn check_exists<'a>(user_input: &'a str, known_commands: &Vec<&str>) -> Result<&'a str, String> {
-    if !known_commands.contains(&user_input.trim_end()) {
-        return Err(format!("{}: command not found", &user_input.trim_end()));
+    if let Ok(cmd) = check_exists(&input, &known_commands) {
+        println!("{}", cmd.join(" "));
     }
-    Ok(user_input)
+}
+fn check_exists<'a>(
+    user_input: &'a str,
+    known_commands: &Vec<&str>,
+) -> Result<Vec<&'a str>, String> {
+    let mut input = user_input.split_whitespace().collect::<VecDeque<&str>>();
+    let cmd = input.pop_front().unwrap();
+    let mut params = Vec::from(input.clone());
+    if !known_commands.contains(&cmd) {
+        return Err(format!("{}: command not found", cmd));
+    }
+    let mut cmd_params = Vec::new();
+    cmd_params.push(cmd);
+    cmd_params.append(&mut params);
+    Ok(cmd_params)
 }
